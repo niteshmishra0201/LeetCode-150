bb69e9f81231b81844f921bb819b7a3ef367b532
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-06-17 11:45:00 +0530

    Reverse array done

---
+++ Arrays/ReverseArray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void reverseArray(vector<int>& arr){
+    int left = 0;
+    int right = arr.size()-1;
+
+    while(left<right){
+        swap(arr[left] , arr[right]);
+        left++;
+        right--;
+    }
+
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    reverseArray(arr);
+    for (int x : arr) cout << x << " ";
+    // Output: 5 4 3 2 1
+    return 0;
+}
