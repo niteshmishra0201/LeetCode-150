9553dd6655481188feaa2f2677ddcd3ade293dd6
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-06-22 10:46:18 +0530

    Fibonacci Number Done

---
+++ Recursion/BasicsOFRecurssion/FibonacciNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fibNumber(int n){
+    //Base Case
+    if(n==0) return 0;
+    if(n==1) return 1;
+
+    return fibNumber(n-1) + fibNumber(n-2);
+}
+
+int main(){
+    fibNumber(5);
+
+    return 0;
+}
