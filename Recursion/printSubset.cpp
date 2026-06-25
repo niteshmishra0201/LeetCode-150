7c6f9fbc4ba4dd26f4260c3c30bd6180c13a6f82
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-06-25 10:30:42 +0530

    Print subset done

---
+++ Recursion/printSubset.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printSubset(int arr[] , int index , int size , vector<int>& current){
+    //Base case
+    if(index == size){
+        cout<< "{";
+        for(int x : current) cout<< x << endl;
+        cout << "}";
+    }
+
+    //choice One - Include the all Element 
+    current.push_back(arr[index]);
+    printSubset(arr , index+1 , size , current);
+
+    // choice second - bactrack and take the second choice of exclude element 
+    current.pop_back();
+    printSubset(arr , index+1 , size , current);
+}
+
+int main(){
+    int arr[] = {1,2,3};
+    vector<int>current;
+    printSubset(arr , 0, 3,current);
+    return 0;
+}
