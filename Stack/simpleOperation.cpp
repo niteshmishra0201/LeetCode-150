59c78a0462445f558c0cac5e75504dacc7fbc295
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-06-26 10:58:54 +0530

    stack starting

---
+++ Stack/simpleOperation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+//This is not any DSA Question , this is just a stack practice for myself
+
+int main(){
+    stack<int> st;
+//Push Operation to add the elemnts into the stack
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+
+    //stack top element and size of the stack 
+    cout<< "stack top Elemrnt" << st.top()<<endl;
+    cout<< "size of the stack"<< st.size()<<endl;
+
+    //Pop operation 
+    st.pop();
+    cout<<"after pop"<<st.top()<<endl;
+
+
+}
