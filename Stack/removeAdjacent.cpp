6e09b964f3a8c9b809311eb90a72a9351051f1c6
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-06-27 10:55:55 +0530

    Remove adajacen done

---
+++ Stack/removeAdjacent.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string removeDuplicates(string s){
+    stack<char> st;
+
+    for(char c:s){
+        if(!st.empty() && st.top()==c){
+            st.pop();
+        }else{
+            st.push(c);
+        }
+    }
+
+    string result = "";
+
+    while(!st.empty()){
+        result += st.top();
+        st.pop();
+    }
+
+    reverse(result.begin() , result.end());
+    return result;
+}
