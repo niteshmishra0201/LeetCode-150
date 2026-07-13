26a4182e8fd349fe5f771a8ec2e6688bb1d8bd19
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-07-13 20:11:07 +0530

    Arranging coins done

---
+++ Binary_Search/ArrangingCoins.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long left = 0;
+        long long right = n;
+        long long ans = 0;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long coins = mid * (mid + 1) / 2;
+
+            if (coins <= n) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
