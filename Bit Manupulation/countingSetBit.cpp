d767eb6ab2a46d1082c5bb5d5a749659eae09483
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-08-04 11:42:49 +0530

    coounting the set bit in number

---
+++ Bit Manupulation/countingSetBit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+
+int countSetBits(int n)
+{
+    int count = 0;
+
+    for(int i = 0; i < 32; i++)
+    {
+        if(n & (1 << i))
+            count++;
+    }
+
+    return count;
+}
