1d897887a71e9ac72db88c6502fd423d8e179515
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-08-15 18:09:43 +0530

    sum of Digit of Number Done

--- Recursion/sumofDIgits.cpp
+++ Recursion/sumofDIgits.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int sumOfDigit(int n){
 
-    //If n == negetive number 
+    
     if(n<0) n=-n;
 
     //Base Case
     if(n==0) return 0;
 
-     // last digit  +  sum of remaining digits
+
     return (n%10) + sumOfDigit(n/10);
 }
 
